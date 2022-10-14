8e85348857edd3a06216c73428a77816ef74f70c
tomf247 <tomfoley247@gmail.com>
2022-10-14 15:52:45 +0000

    Heroku deployment

--- project/settings.py
+++ project/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['tf-sharetracker.herokuapp.com', 'localhost',]
 
 CSRF_TRUSTED_ORIGINS = ['localhost']
 
