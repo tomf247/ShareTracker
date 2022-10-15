6cf04f8a6246a95fbb4257bddff1832ee8239ca0
tomf247 <tomfoley247@gmail.com>
2022-10-15 22:39:26 +0000

    Add Change and Delete CRUD functionality

--- project/urls.py
+++ project/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', views.ViewTrades.as_view(), name="viewtrades"),
     path('createtrade/', views.CreateTrade.as_view(), name="createtrade"),
+    path('updatetrade/<int:pk>', views.UpdateTrade.as_view(), name="updatetrade"),
+    path('deletetrade/<int:pk>/', views.DeleteTrade.as_view(), name="deletetrade"),
 ]
--- trades/views.py
+++ trades/views.py
@@ -2,12 +2,12 @@ from trades.models import Trade
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from trades.forms import TradeForm
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse_lazy
 from django.contrib import messages
 
 class ViewTrades (LoginRequiredMixin, ListView):
-    ''' Display a list of all shares purchased for the logged in user '''
+    ''' Display a list of all trades in the user's portfolio. '''
 
     model = Trade
     paginate_by = 6
@@ -16,19 +16,71 @@ class ViewTrades (LoginRequiredMixin, ListView):
 
         return Trade.objects.filter(user=self.request.user)
 
+
 class CreateTrade(CreateView):
-    ''' Record a new share purchase '''
+    ''' Record a new trade. '''
 
     model = Trade
     form_class = TradeForm
 
     def form_valid(self, form):
-        ''' Ensure the trade is saved to the user's account  '''
+        ''' Ensure the trade is saved to the user's (not someone else's) account. '''
+
         self.object = form.save(commit=False)
         self.object.user = self.request.user
         self.object.save()
         return HttpResponseRedirect(self.get_success_url())
 
     def get_success_url(self):
-        messages.success(self.request, 'Trade created successfully')
+        ''' Notify the Create was successful and return to the ListView '''
+
+        messages.success(self.request, 'Trade created successfully.')
         return reverse_lazy('viewtrades')
+
+
+class UpdateTrade(UpdateView):
+    ''' Make changes to a previously inserted trade. '''
+
+    model = Trade
+    form_class = TradeForm
+
+    def dispatch(self, request, *args, **kwargs):
+        ''' Override to ensure the user can only change their trades. '''
+
+        handler = super(UpdateTrade, self).dispatch(request, *args, **kwargs)
+        if self.object.user != request.user:
+            raise Http404('You tried to update a trade that is not in your portfolio.')
+        return handler
+
+    def form_valid(self, form):
+        ''' Save the changes back to the database. '''
+
+        self.object = form.save(commit=False)
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        ''' Notify the Update was successful and return to the ListView '''
+
+        messages.success(self.request, 'Trade updated successfully.')
+        return reverse_lazy('viewtrades')
+
+
+class DeleteTrade(DeleteView):
+    ''' Remove a trade from user's portfolio. '''
+    model = Trade
+    success_url = reverse_lazy('viewtrades')
+
+    def dispatch(self, request, *args, **kwargs):
+        ''' Override to ensure the user can only delete their trades. '''
+        handler = super(DeleteTrade, self).dispatch(request, *args, **kwargs)
+        if self.object.user != request.user:
+            raise Http404('You tried to delete a trade that is not in your portfolio.')
+        return handler
+    
+    def delete(self, request, *args, **kwargs):
+        ''' Override to raise notification of successful delete. '''
+        messages.success(self.request, 'Trade deleted successfully.')
+
+        return super(DeleteTrade, self).delete(request, *args, **kwargs)
+    
